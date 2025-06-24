bdf99e5a9141f8453f25433b91d4295325e310b4
CharlieB <charlie@superiordigital.design>
2025-06-24 10:15:27 -0300

    basic home layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+import "./globals.css"; // Tu archivo CSS global de Tailwind
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
 export const metadata: Metadata = {
-  title: "Light-IT Care",
-  description: "Light-IT Management System",
+  title: "HealthCare Company",
+  description: "Your health, our priority.",
 };
 
 export default function RootLayout({
@@ -23,11 +14,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`
+         
+          flex flex-col min-h-screen 
+        `}
       >
-        {children}
+        <Header />
+
+        <main className="flex-grow">{children}</main>
+
+        <Footer />
       </body>
     </html>
   );
