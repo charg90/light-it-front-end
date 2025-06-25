e96eb8a46afa55a2d648940ee4829845adfd9c29
CharlieB <charlie@superiordigital.design>
2025-06-25 10:39:29 -0300

    fix:when there are no patients show message

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,9 +7,19 @@ import DashboardLoading from "./loading";
 async function dashboard() {
   const patients = await getPatients();
 
+  if (patients.length === 0) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <p className="text-gray-500 text-lg">
+          No patients found. Please add a patient.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <Suspense fallback={<DashboardLoading />}>
-      <ClienteDashboard initialPatients={patients} />;
+      <ClienteDashboard initialPatients={patients} />
     </Suspense>
   );
 }
